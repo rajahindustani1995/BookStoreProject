48d41204aa3b5e8eabb15ede3824debeb7ca404c
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-11-07 01:52:24 +0530

    [Rajesh] Added : Wishlist Component Added

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/Service/userService/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder, private user: UserService, private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    })
+  }
+
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.valid) {
+      console.log("Registration Successful")
+      let reqdata = {
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password,
+      }
+      this.user.login(reqdata).subscribe((response: any) => {
+        console.log(response)
+        localStorage.setItem("token", response.data)
+        this.router.navigateByUrl("home/books");
+      }, error => {
+        console.log(error)
+      }
+      )
+    }
+
+
   }
 
 }
--- src/app/component/registration/registration.component.ts
+++ src/app/component/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/Service/userService/user.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  registerForm! : FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder , private user: UserService) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      fullName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      phone: ['', Validators.required]
+    })
   }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit(){
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.valid) {
+        console.log("Registration Successful")
+        let reqdata ={
+          fullName: this.registerForm.value.fullName,
+          email: this.registerForm.value.email,
+          password: this.registerForm.value.password,
+          phone: this.registerForm.value.phone,
+        }
+        this.user.registration(reqdata).subscribe((response:any) => {
+          console.log(response)
+  
+        }, error => {
+          console.log(error)
+        }
+        )
+      }
+  
+      
+    }
 
 }
